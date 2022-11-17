31884be53de48826f694084dc02e557d3b783fb6
Spythere <spythere@protonmail.com>
2022-11-17 13:07:25 +0100

    Dodano Swagger

--- src/api/dto/getProducts.dto.ts
+++ src/api/dto/getProducts.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class GetProductsDto {
+  @ApiProperty({
+    type: String,
+    description: 'Category name',
+    required: true,
+  })
   @IsString()
   category: string;
 }
