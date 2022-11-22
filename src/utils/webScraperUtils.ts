9feffdfed4b06827ad3837c98fa972aae901067c
Spythere <spythere@protonmail.com>
2022-11-22 18:28:03 +0100

    Puppeteer args

--- src/utils/webScraperUtils.ts
+++ src/utils/webScraperUtils.ts
@@ -2,8 +2,7 @@ import * as puppeteer from 'puppeteer';
 
 export const connectToURL = async (url: string) => {
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
 
