c933aef0a2e3fc09ceae702470da49936e4ad2d3
Spythere <spythere@protonmail.com>
2022-11-22 18:16:07 +0100

    Poprawka puppeteer

--- src/utils/webScraperUtils.ts
+++ src/utils/webScraperUtils.ts
@@ -1,7 +1,10 @@
 import * as puppeteer from 'puppeteer';
 
 export const connectToURL = async (url: string) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
 
   await page.goto(url);
