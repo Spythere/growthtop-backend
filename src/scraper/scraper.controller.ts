e2118952be2b5f83d31d1955235d982caea2a841
Spythere <spythere@protonmail.com>
2022-11-24 00:11:45 +0100

    Przywrócenie scrapera na produkcję

--- src/scraper/scraper.controller.ts
+++ src/scraper/scraper.controller.ts
@@ -19,7 +19,8 @@ export class ScraperController implements OnModuleInit {
   ) {}
 
   onModuleInit() {
-    if (process.env.NODE_ENV === 'development') this.fetchPageData();
+    // if (process.env.NODE_ENV === 'development') 
+    this.fetchPageData();
   }
 
   async fetchPageData() {
