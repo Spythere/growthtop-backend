06de3d88b9556cce3e843d478786eb251c19e0cd
Spythere <spythere@protonmail.com>
2022-11-24 00:11:09 +0100

    Optymalizacja scrapera

--- src/utils/webScraperUtils.ts
+++ src/utils/webScraperUtils.ts
@@ -3,7 +3,16 @@ import * as puppeteer from 'puppeteer';
 export const createBrowser = async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu',
+    ],
   });
 
   const page = await browser.newPage();
